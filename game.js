3d1ec606eaf5424cc352cfdca74f0f4b3a7ecdfc
spacegeek224 <marisusisme@gmail.com>
2015-10-09 20:31:46 +0000

    First commit

---
+++ game.js
@@ -0,0 +1,13 @@
+/**
+* Created with pixelventure.
+* User: spacegeek224
+* Date: 2015-10-09
+* Time: 08:23 PM
+* To change this template use Tools | Templates.
+*/
+var canvas = CE.defines('canvas_id').ready(function() {
+    //Ready
+});
+
+canvas.Scene.new
+
